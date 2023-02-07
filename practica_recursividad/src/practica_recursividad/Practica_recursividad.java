13f0ecdbdfbe59075caf6e8d03808a27624ef702
andrewbocker <58228967+andrewbocker@users.noreply.github.com>
2023-02-06 18:56:54 -0600

    Update Practica_recursividad.java

--- practica_recursividad/src/practica_recursividad/Practica_recursividad.java
+++ practica_recursividad/src/practica_recursividad/Practica_recursividad.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package practica_recursividad;
-
+import javax.swing.JOptionPane;
 /**
  *
  * @author andre
@@ -14,7 +14,32 @@ public class Practica_recursividad {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int opcion = 0;
+        
+        while (opcion != 5){
+             opcion = Integer.parseInt(JOptionPane.showInputDialog(null, 
+                     "Ingrese el numero de la opcion que desea escoger: "
+                     + "1. Ejercicio 1"
+                     + "2. Ejercicio 2"
+                     + "3. Ejercicio 3"
+                     + "4. Ejercicio 4"
+                     + "5. Salir"));
+             
+             if (opcion == 1){
+             } else if (opcion == 2){
+                 
+             } else if (opcion == 3){
+                 
+             } else if (opcion == 4){
+                 
+             } else if (opcion == 5){
+                 JOptionPane.showMessageDialog(null, "Ha "
+                         + "seleccionado la opcion de salir, muchas gracias");
+             } else {
+                 JOptionPane.showMessageDialog(null, "Opcion "
+                         + "no valida, ingrese otra opcion");
+             }
+        }
     }
     
 }
