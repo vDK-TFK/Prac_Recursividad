93b8b01f900ba1a169ac31d3f54dd622e8aa3a4c
andrewbocker <58228967+andrewbocker@users.noreply.github.com>
2023-02-06 18:54:50 -0600

    Add files via upload

---
+++ practica_recursividad/src/practica_recursividad/Practica_recursividad.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practica_recursividad;
+
+/**
+ *
+ * @author andre
+ */
+public class Practica_recursividad {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
