17949b311857e6e4059a038fa0d9cf65ae7312f6
vDK-TFK <113957562+vDK-TFK@users.noreply.github.com>
2023-02-09 22:48:24 -0600

    Agregue el ejercicio 4

--- practica_recursividad/src/practica_recursividad/Practica_recursividad.java
+++ practica_recursividad/src/practica_recursividad/Practica_recursividad.java
@@ -3,7 +3,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package practica_recursividad;
+
 import javax.swing.JOptionPane;
+
 /**
  *
  * @author andre
@@ -15,31 +17,41 @@ public class Practica_recursividad {
      */
     public static void main(String[] args) {
         int opcion = 0;
-        
-        while (opcion != 5){
-             opcion = Integer.parseInt(JOptionPane.showInputDialog(null, 
-                     "Ingrese el numero de la opcion que desea escoger: "
-                     + "1. Ejercicio 1"
-                     + "2. Ejercicio 2"
-                     + "3. Ejercicio 3"
-                     + "4. Ejercicio 4"
-                     + "5. Salir"));
-             
-             if (opcion == 1){
-             } else if (opcion == 2){
-                 
-             } else if (opcion == 3){
-                 
-             } else if (opcion == 4){
-                 
-             } else if (opcion == 5){
-                 JOptionPane.showMessageDialog(null, "Ha "
-                         + "seleccionado la opcion de salir, muchas gracias");
-             } else {
-                 JOptionPane.showMessageDialog(null, "Opcion "
-                         + "no valida, ingrese otra opcion");
-             }
+
+        while (opcion != 5) {
+            opcion = Integer.parseInt(JOptionPane.showInputDialog(null,
+                    "Ingrese el numero de la opcion que desea escoger: \n"
+                    + "1. Ejercicio 1\n"
+                    + "2. Ejercicio 2\n"
+                    + "3. Ejercicio 3\n"
+                    + "4. Ejercicio 4\n"
+                    + "5. Salir"));
+
+            if (opcion == 1) {
+            } else if (opcion == 2) {
+
+            } else if (opcion == 3) {
+
+            } else if (opcion == 4) {
+                Ejercicio4 c = new Ejercicio4();
+                int exponencial = (int) c.calcExponencial(5, 2);
+                int exponencialRec = (int) c.caclExponencialRecu(5, 2);
+                int factorial = (int) c.factorial(5);
+                int factorialRec = (int) c.factorialrecu(5);
+                System.out.printf("El valor que da al calcular exponencial sin recursividad es: %d\n", exponencial);
+                System.out.printf("El valor que da al calcular factorial sin recursividad es: %d\n", factorial);
+                System.out.printf("========= Ahora con recursividad =========\n");
+                System.out.printf("El valor que da al calcular exponencial con recursividad es: %d\n", exponencialRec);
+                System.out.printf("El valor que da al calcular factorial con recursividad es: %d\n", factorialRec);
+
+            } else if (opcion == 5) {
+                JOptionPane.showMessageDialog(null, "Ha "
+                        + "seleccionado la opcion de salir, muchas gracias");
+            } else {
+                JOptionPane.showMessageDialog(null, "Opcion "
+                        + "no valida, ingrese otra opcion");
+            }
         }
     }
-    
+
 }
